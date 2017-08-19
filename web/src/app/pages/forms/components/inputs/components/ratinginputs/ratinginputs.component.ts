e13dff8350177d33c8ef73fdfc7841a19d637fa2
Thomas Tong <mankintong@gmail.com>
2017-08-19 17:04:44 +0800

    sample range input

--- web/src/app/pages/forms/components/inputs/components/ratinginputs/ratinginputs.component.ts
+++ web/src/app/pages/forms/components/inputs/components/ratinginputs/ratinginputs.component.ts
@@ -8,9 +8,11 @@ import { Component } from '@angular/core';
 export class Rating {
   public _rate1:number = 3;
   public _rate2:number = 4;
+  public _rate3:number = 50;
 
   public _max1:number = 5;
   public _max2:number = 10;
+  public _max3:number = 100;
 
   constructor() {
   }
